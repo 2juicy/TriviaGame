d62cb1927e0c91f9886a6d5beb9b1f8979364608
2Juicy <j0hnnywchiang@gmail.com>
2018-05-16 12:50:55 -0700

    buggy answerCheck needs fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,9 +24,98 @@ const q5 = {
     choice: ['6', '5', '4', '3'],
 };
 const qList = [q1, q2, q3, q4 ,q5];
+var count = 0;
 var score = 0;
+var userGuess = 0;
+var end = false;
 //Document check
 $(document).ready(function() {
+    var nextQuest = function(x){
+        let y = qList[x];
+        if (y === q1){
+            $('#yourQuestion').text(q1.question);
+            $('#1A').text(q1.choice[0]);
+            $('#2A').text(q1.choice[1]);
+            $('#3A').text(q1.choice[2]);
+            $('#4A').text(q1.choice[3]);
+        } else if (y === q2){
+            $('#yourQuestion').text(q2.question);
+            $('#1A').text(q2.choice[0]);
+            $('#2A').text(q2.choice[1]);
+            $('#3A').text(q2.choice[2]);
+            $('#4A').text(q2.choice[3]);
+        } else if (y === q3){
+            $('#yourQuestion').text(q3.question);
+            $('#1A').text(q3.choice[0]);
+            $('#2A').text(q3.choice[1]);
+            $('#3A').text(q3.choice[2]);
+            $('#4A').text(q3.choice[3]);
+        } else if (y === q4){
+            $('#yourQuestion').text(q4.question);
+            $('#1A').text(q4.choice[0]);
+            $('#2A').text(q4.choice[1]);
+            $('#3A').text(q4.choice[2]);
+            $('#4A').text(q4.choice[3]);
+        } else {
+            $('#yourQuestion').text(q5.question);
+            $('#1A').text(q5.choice[0]);
+            $('#2A').text(q5.choice[1]);
+            $('#3A').text(q5.choice[2]);
+            $('#4A').text(q5.choice[3]);
+        }
+    }
+    var answerCheck = function(x){
+        if (q1.choice[x] === q1.answer){
+            $("#combatText").text('You are correct!');
+            score++;
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (count === 0 && q1.choice[x] !== q1.answer){
+            $("#combatText").text('You are incorrect! the correct answer is ' + q1.answer);
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (q2.choice[x] === q2.answer){
+            $("#combatText").text('You are correct!');
+            score++;
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (count === 1 && q2.choice[x] !== q2.answer){
+            $("#combatText").text('You are incorrect! the correct answer is ' + q2.answer);
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (q3.choice[x] === q3.answer){
+            $("#combatText").text('You are correct!');
+            score++;
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (count === 2 && q3.choice[x] !== q3.answer){
+            $("#combatText").text('You are incorrect! the correct answer is ' + q3.answer);
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (q4.choice[x] === q4.answer){
+            $("#combatText").text('You are correct!');
+            score++;
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (count === 3 && q4.choice[x] !== q4.answer) {
+            $("#combatText").text('You are incorrect! the correct answer is ' + q4.answer);
+            $('#score').text('Your score: ' + score);
+            count++;            
+        } else if (q5.choice[x] === q5.answer){
+            $("#combatText").text('You are correct!');
+            score++;
+            $('#score').text('Your score: ' + score);
+            count++;            
+            end = true;
+        } else if (count === 4 && q5.choice[x] !== q5.answer){
+            $("#combatText").text('You are incorrect! the correct answer is ' + q5.answer);
+            $('#score').text('Your score: ' + score);
+            count++;            
+            end = true;    
+        } else {
+            return;
+        }
+    }
     $('.answer').hide();
     $('#yourQuestion').hide();
     //click event to start game
@@ -35,24 +124,20 @@ $(document).ready(function() {
         $('#start').hide();
         $('.answer').fadeIn(1000);
         $('#yourQuestion').fadeIn(1000);
-        //Questions appearing on screen need for loop.
-        $('#yourQuestion').text(q1.question);
-        $('#1A').text(q1.choice[0]);
-        $('#2A').text(q1.choice[1]);
-        $('#3A').text(q1.choice[2]);
-        $('#4A').text(q1.choice[3]);
+        nextQuest(0);
     });
         //onclick for answer and validation of correct answer.
     $(".answer").click(function(){
-        let userPick = $(this).val();
-        if (q1.choice[userPick] === q1.answer){
-            $("#combatText").text('You are correct!');
-        } else {
-            $("#combatText").text('You are incorrect! the correct answer is ' + q1.answer);
+        userPick = $(this).val();
+        answerCheck(userPick);
+        nextQuest(count);
+        if (end === true){
+            $('.answer').hide();
+            $('#yourQuestion').hide();
         }
 
 
-// setTimeout(fiveSeconds, 1000 * 5);
+// setTimeout(functionHere, 1000 * 5);
 
 
 
